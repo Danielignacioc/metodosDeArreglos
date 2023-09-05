8272ac4999fe5fe2d997808740f4ed3611a354b6
Danielignacioc <carcamodaniel.informatica@gmail.com>
2023-09-05 01:31:11 -0300

    vars name and utility refurbished

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,64 @@
+document.addEventListener("DOMContentLoaded", main(false)) ;
+
+function main(validated) {
+  let hr = new Date();
+  console.log(hr);
+  
+  const task = document.querySelector('#addTaskTxt');
+ 
+
+  if ((validated != 'false') || (validated != 0) || (validated != undefined )|| (validated != null) ) {
+  
+    console.log('[+][+][+]FUNCION CARGADA[+][+][+]');
+    console.log(task);
+    }
+    
+    else {
+      val(task, validated)
+      validated = false;
+      return (task, validated);
+    }
+    
+    function val(task, validated) {
+
+    if ((task.value != '') || (task.value != undefined) || (task.value != null)) {
+      validated = true;
+      return(task, validated);
+    }
+
+    else {
+      validated = false;
+      return main(validated);
+    }
+
+  }
+
+
+}
+
+
+
+
+
+
+
+
+//FUNCION INNECESARIA 
+/* task.addEventListener('', getData());
+  function getData(task)
+  {
+    let formatdata = [];                          
+    formatdata 
+    return formatData;
+  } 
+}
+
+
+*/
+
+
+/* const inTarea = document.querySelector("input");
+
 const tareas = [
   { id: 1, task: "primera tarea", status: false },
   { id: 2, task: "segunda tarea", status: false },
@@ -9,13 +70,12 @@ const tareas = [
 
 function agregarTarea() {
 const spanHtml = document.querySelector("#inner_texto");
-const inTarea = document.querySelector("input");
 
   let htmlRow = "";
   let con = 3;
 
 
-  for (let tarea of tareas) {
+  for (let tarea of inTareas) {
     con++;
     htmlRow += `
     <tr>
@@ -34,4 +94,13 @@ const boton = document.querySelector("#nuevaTarea");
 boton.addEventListener("click", () => {
   tareas.push(con, inTarea.value, true);
   agregarTarea();
-});
+});}
+ */
+
+
+/* const inTarea = document.getElementsByTagName("input");
+tskVal = inTarea[0].value;
+
+console.log(inTarea[0].checkValidity);  /* crear funcion que agregue la propiedad checked y ocuparla para validar */
+
+
